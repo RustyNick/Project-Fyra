3eab3f0ed637e451012b1e0e0c22daa630af0225
Rusty-nick <rusty-nick@live.com>
2021-06-08 15:27:16 +0200

    save code progress calender

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TodoList from './todo/TodoList'
 import Calendar from "./components/Calendar";
 
@@ -7,35 +7,38 @@ const style = {
   margin: "50px auto"
 }
 
+
+
 function App() {
   let todos = [
-    {id: 1, complated: false, title: 'måndag'},
-    {id: 2, complated: false, title: 'tisdag'},
-    {id: 3, complated: false, title: 'onsdag'},
-    {id: 4, complated: false, title: 'torsdag'},
-    {id: 5, complated: false, title: 'fredag'}
+    { id: 1, complated: false, title: 'måndag' },
+    { id: 2, complated: false, title: 'tisdag' },
+    { id: 3, complated: false, title: 'onsdag' },
+    { id: 4, complated: false, title: 'torsdag' },
+    { id: 5, complated: false, title: 'fredag' }
   ]
 
-function toggleTodo(id) {
-/*   console.log('todo id', id),
- */  todos = todos.map(todo=>{
-    if(todo.id===id){
-      todo.complated=!todo.complated
-    }
-    return todo
-  })
-}
+  function toggleTodo(id) {
+    /*   console.log('todo id', id),*/
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.complated = !todo.complated
+      }
+      return todo
+    })
+  }
 
   return (
     <div className="wrapper">
-      <div className="App">
-        <Calendar style={style} width="350px" />
-      </div>
       <h1>Trello</h1>
       < TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+      <div className="App">
+        <Calendar style={style} width="350px"
+          onDayClick={(e, day) => this.onDayClick(e, day)} />
+      </div>
     </div>
   );
-
 }
 
 export default App;
+
