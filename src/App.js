3de99c1876216a1ef6636ac5f9725bb0e25fe265
Rusty-nick <rusty-nick@live.com>
2021-06-08 09:21:14 +0200

    save niklas calender progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import TodoList from './todo/TodoList'
+import Calendar from "./components/Calendar";
+
+const style = {
+  position: "relative",
+  margin: "50px auto"
+}
+
 function App() {
   return (
     <div className="wrapper">
+      <div className="App">
+        <Calendar style={style} width="350px" />
+      </div>
       <h1>Trello</h1>
       < TodoList></TodoList>
     </div>
   );
+
 }
 
 export default App;
