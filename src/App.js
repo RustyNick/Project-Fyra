2fcc4f288b6a1e9609bf5eca017078489b6bd77f
Kanan <kenan@luxar.se>
2021-06-08 11:24:30 +0200

    DONE knapp och Checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import TodoList from './Todo/TodoList'
+import Context from './context'
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, complated: false, title: 'handla'},
     {id: 2, complated: false, title: 'lämna böker'},
     {id: 3, complated: false, title: 'klippa gräs'},
-  ]
+  ])
+  
 
 function toggleTodo(id) {
 /*   console.log('todo id', id),
- */  todos = todos.map(todo=>{
+ */  setTodos (
+   todos.map(todo=>{
     if(todo.id===id){
       todo.complated=!todo.complated
     }
     return todo
   })
+  )
 }
-
+function removeTodo (id){
+setTodos(todos.filter(todo => todo.id !== id ))
+}
   return (
+    <Context.Provider value={{removeTodo}}>
     <div className="main">
       <div className="list">
           <h5>Lista A</h5>
@@ -29,7 +36,8 @@ function toggleTodo(id) {
       </div>
         <button className="newList">Skapa ny lista</button>
     </div>
-  );
+  </Context.Provider>
+  )
 }
 
 export default App;
--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PropTypes from 'prop-types'
+/* import classes from '*.module.css' */
+import Context from '../context'
 
 
 function TodoItem ({ todo, index, onChange }) {
-    console.log('todo', todo)
+    /* console.log('todo', todo) */
+    const {removeTodo} = useContext(Context)
+    const classes = []
+    if (todo.complated){
+        classes.push('done')
+    }
     return (
         <li>
-            <span><div className='kort'>
-            <input type='checkbox' onChange={() => onChange(todo.id) /* , console.log(todo.id) */}/>
+            <span className={classes.join(' ')}>
+            <div className='kort'><input 
+            type='checkbox'
+            checked={todo.complated}
+            onChange={() => onChange(todo.id) 
+                /* , console.log(todo.id) */}/>
             <strong>{ index + 1 + '. '}</strong>
             {todo.title}
             <div className='buttons'>
-            <button className='done'>&times;<span>DONE</span></button>
+            <button className='doneBtn' onClick={removeTodo.bind(null, todo.id)/* () => removeTodo(todo.id) */}><span>DONE</span></button>
             </div>
             </div>
             
