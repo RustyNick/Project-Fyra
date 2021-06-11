4281725fa4882002684259401bdd9f9e41726032
Kanan <kenan@luxar.se>
2021-06-11 15:33:44 +0200

    Sync kod

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ToDoRender from './Todo/ToDoRender';
-import React, { Component, useEffect } from 'react'
-import Context from './context'
+//import React, { Component, useEffect } from 'react'
+//import Context from './context'
 import Calendar from "./components/Calendar";
 
 const style = {
@@ -11,12 +11,16 @@ const style = {
 
 function App() {
   
-    return (
-    <ToDoRender/>
-    <div className="App">
-        <Calendar style={style} width="350px"
-          onDayClick={(e, day) => this.onDayClick(e, day)} />
+  return (
+    <div>
+      <ToDoRender/>,
+
+   <div className="App">
+    <Calendar style={style} width="350px"
+    onDayClick={(e, day) => this.onDayClick(e, day)} />
+    
       </div>
+    </div>
   )
 }
 
