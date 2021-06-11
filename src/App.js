eb8b1f283ae55dea221353687f464ef73b4eecc0
Kanan <kenan@luxar.se>
2021-06-11 10:52:41 +0200

    Flytad koden från App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ToDoRender from './Todo/ToDoRender';
+import React, { Component, useEffect } from 'react'
+import Context from './context'
+import Calendar from "./components/Calendar";
+
+const style = {
+  posistion: "relative",
+  margin: "50px auto"
+}
 
 function App() {
   
     return (
     <ToDoRender/>
+    <div className="App">
+        <Calendar style={style} width="350px"
+          onDayClick={(e, day) => this.onDayClick(e, day)} />
+      </div>
   )
 }
 
